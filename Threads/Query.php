f8ebb620fbf52e27898b71f8a0e51cf1356ff085
satz <sathish.thi@gmail.com>
2017-05-05 12:13:28 +0530

    Added Origianl mail for testing

--- Threads/Query.php
+++ Threads/Query.php
@@ -21,7 +21,6 @@ class Query extends \Threaded
         print_r(count($results));
         try {
             $mail = new \PHPMailer();
-            print_r($mail);
             //$mail->SMTPDebug = 3;                               // Enable verbose debug output
             $mail->isSMTP();                                      // Set mailer to use SMTP
             $mail->Host = 'node4.mailpixels.com';  // Specify main and backup SMTP servers
@@ -30,16 +29,25 @@ class Query extends \Threaded
             $mail->Password = 'spiceuser';                           // SMTP password
             $mail->SMTPSecure = 'tls';                            // Enable TLS encryption, `ssl` also accepted
             $mail->Port = 587;                                    // TCP port to connect to
-            $mail->setFrom('no-reply@freshworker.com', 'Mailer');
+            $mail->setFrom('no-reply@freshworkes.com', 'Mailer');
             $mail->isHTML(true);                                  // Set email format to HTML
             $mail->Subject = 'Here is the subject';
             $mail->Body = 'This is the HTML message body <b>in bold!</b>';
             $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
-            foreach ($results as $result) {
-                $mail->addAddress($result['email'], $result['name']);
-                print_r($result);
-                $mail->send();
+            $mail->addAddress('sathish.thi@gmail.com','sathish');
+            if(!$mail->send()){
+                print_r("Mailer Error: " . $mail->ErrorInfo);
             }
+            else{
+                print_r('Mail Sent');
+            }
+            $mail->ClearAddresses();
+           /* foreach ($results as $result) {
+                $mail->addAddress($result['email'], $result['name']);
+                $mail->send();
+                $mail->ClearAddresses();
+                $mail->ClearAttachments();
+            } */
         }
         catch (\Exception $e) {
             print_r($e);
